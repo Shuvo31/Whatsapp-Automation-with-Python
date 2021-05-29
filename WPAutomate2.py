f3825bd41f4a0060d3c43dd03bb5d8f26aa732a8
Shuvojit Das <Shuvojitdas2000@gmail.com>
2021-05-29 20:44:09 +0530

    Create WPAutomate2.py

---
+++ WPAutomate2.py
@@ -0,0 +1,6 @@
+import pywhatkit 
+try:
+  pywhatkit.sendwhatmsg('+91 xxxxxxxxxx','Hi! Beautiful i love u with my whole heart',20,50)
+  print("Message sent successfully!")
+except:
+  print("Some error has occured!")
